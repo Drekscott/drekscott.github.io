7708bb589f9aade32dc1ec74b8b13e9db85a2356
Dreyton Scott <drekscott@icloud.com>
2018-02-13 21:28:31 -0500

    Changed portfolio

--- src/js/app.js
+++ src/js/app.js
@@ -13,13 +13,13 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#FFD700"
       },
       "shape": {
         "type": "polygon",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#FFD700"
         },
         "polygon": {
           "nb_sides": 5
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#FFD700",
         "opacity": 0.4,
         "width": 1
       },
